e502d5b8f1f3c26a92a9f56a07075b05fb4d08fc
Alan <a110082037@mail.shu.edu.tw>
2022-11-15 18:11:23 +0800

    新增第二場景，編寫場景切換程式

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Event : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene currScene = SceneManager.GetActiveScene();
+        GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        int ecount = enemys.Length;
+        if (ecount == 0)
+        {
+            if (currScene.buildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else if (currScene.buildIndex == 1)
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        }
+    }
+}
